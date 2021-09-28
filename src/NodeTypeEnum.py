62302f01caac4600e8a21c3ef8beb23dcfe3d896
ondrej33 <75424794+ondrej33@users.noreply.github.com>
2021-09-28 16:33:25 +0200

    added enum for node category to the tree, not used yet

---
+++ src/NodeTypeEnum.py
@@ -0,0 +1,57 @@
+from enum import Enum, auto
+
+
+class NodeType(Enum):
+    PROP = auto()
+    VAR = auto()
+    TRUE = auto()
+    FALSE = auto()
+
+    NEG = auto()
+    DIS = auto()
+    CON = auto()
+    IMPL = auto()
+    EQIV = auto()
+
+    EX = auto()
+    AX = auto()
+    EF = auto()
+    AF = auto()
+    EU = auto()
+    AU = auto()
+    EG = auto()
+    AG = auto()
+    EW = auto()
+    AW = auto()
+    EY = auto()
+    AY = auto()
+
+    BIND = auto()
+    JUMP = auto()
+    EXIST = auto()
+
+
+OP_DICT = {
+    "~": NodeType.NEG,
+    "||": NodeType.DIS,
+    "&&": NodeType.CON,
+    "->": NodeType.IMPL,
+    "<->": NodeType.EQIV,
+
+    "EX": NodeType.EX,
+    "AX": NodeType.AX,
+    "EF": NodeType.EF,
+    "AF": NodeType.AF,
+    "EG": NodeType.EG,
+    "AG": NodeType.AG,
+    "EU": NodeType.EU,
+    "AU": NodeType.AU,
+    "EW": NodeType.EW,
+    "AW": NodeType.AW,
+    "EY": NodeType.EY,
+    "AY": NodeType.AY,
+
+    "!": NodeType.BIND,
+    "@": NodeType.JUMP,
+    "3": NodeType.EXIST,
+}
--- src/abstract_syntax_tree.py
+++ src/abstract_syntax_tree.py
@@ -1,10 +1,14 @@
 # TODO: use some inheritance and merge some code
 
+from NodeTypeEnum import NodeType, OP_DICT
+
+
 class Node:
     def __init__(self, value):
         self.value = value
         self.height = None
         self.subform_string = None
+        self.category = None
 
     # just define something so that heapq can be used on nodes
     def __lt__(self, other):
@@ -16,6 +20,17 @@ class TerminalNode(Node):
         super().__init__(value)
         self.subform_string = value
         self.height = 1
+        self.category = self.classify_terminal()
+
+    def classify_terminal(self):
+        if self.value in {"True", "true", "tt"}:
+            return NodeType.TRUE
+        elif self.value in {"False", "false", "ff"}:
+            return NodeType.FALSE
+        elif '{' in self.value:
+            return NodeType.VAR
+        else:
+            return NodeType.PROP
 
 
 class UnaryNode(Node):
@@ -24,6 +39,7 @@ class UnaryNode(Node):
         self.child = child
         self.subform_string = "(" + value + child.subform_string + ")"
         self.height = child.height + 1
+        self.category = OP_DICT[value]
 
 
 class BinaryNode(Node):
@@ -33,6 +49,7 @@ class BinaryNode(Node):
         self.right = right
         self.subform_string = "(" + left.subform_string + value + right.subform_string + ")"
         self.height = left.height + 1 if left.height > right.height else right.height + 1
+        self.category = OP_DICT[value]
 
 
 class HybridNode(Node):
@@ -42,3 +59,4 @@ class HybridNode(Node):
         self.child = child
         self.subform_string = "(" + value + var + ":" + child.subform_string + ")"
         self.height = child.height + 1
+        self.category = OP_DICT[value]
