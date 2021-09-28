0b0a0bae32d51f4ab847977a96e0ac54124033b5
ondrej33 <75424794+ondrej33@users.noreply.github.com>
2021-09-28 16:34:35 +0200

    added enum for node category to the tree, not used yet

--- src/NodeTypeEnum.py
+++ src/NodeTypeEnum.py
@@ -33,8 +33,9 @@ class NodeType(Enum):
 
 OP_DICT = {
     "~": NodeType.NEG,
-    "||": NodeType.DIS,
+    "|": NodeType.DIS,
     "&&": NodeType.CON,
+    "&": NodeType.CON,
     "->": NodeType.IMPL,
     "<->": NodeType.EQIV,
 
