eaf203c54e4d26a346f0b9e46bcb3281ff0cc5b3
Ondřej Huvar <492849@mail.muni.cz>
2022-05-13 20:34:42 +0200

    Update eval script for single-coloured benchmarks

--- script_eval_monochromatic.sh
+++ script_eval_monochromatic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-project_dir="."
-models_dir="./benchmark_models/model_collection_large/"
+src_dir="./src"
+models_dir="./benchmark_models/model_collection_large"
 
 for m in `ls ${models_dir}`
 do
@@ -20,14 +20,14 @@ do
     # for models with inputs we consider the version with all inputs fixed to 0
 
     if [ -f "${models_dir}/${m}/model_inputs_false.bnet" ]; then
-        timeout -v 1h python3 "${project_dir}/src/model_check.py" "${models_dir}/${m}/model_inputs_false.bnet" 'AF !{x}: (AX (~{x} && AF {x}))'
-        timeout -v 1h python3 "${project_dir}/src/model_check.py" "${models_dir}/${m}/model_inputs_false.bnet" 'AF !{x}: ((AX (~{x} && AF {x})) && (EF !{y}: EX ~AF {y}))'
-        timeout -v 1h python3 "${project_dir}/src/model_check.py" "${models_dir}/${m}/model_inputs_false.bnet" '!{x}: 3{y}: (@{x}: ~{y} && AX {x}) && (@{y}: AX {y})'
-        timeout -v 1h python3 "${project_dir}/src/model_check.py" "${models_dir}/${m}/model_inputs_false.bnet" '3{x}: 3{y}: (@{x}: ~{y} && AX {x}) && (@{y}: AX {y}) && EF ({x} && !{z}: AX {z}) && EF ({y} && !{z}: AX {z}) && AX (EF ({x} && !{z}: AX {z}) ^ EF ({y} && !{z}: AX {z}))'
+        timeout -v 1h python3 "${src_dir}/model_check.py" "${models_dir}/${m}/model_inputs_false.bnet" 'AF !{x}: (AX (~{x} && AF {x}))'
+        timeout -v 1h python3 "${src_dir}/model_check.py" "${models_dir}/${m}/model_inputs_false.bnet" 'AF !{x}: ((AX (~{x} && AF {x})) && (EF !{y}: EX ~AF {y}))'
+        timeout -v 1h python3 "${src_dir}/model_check.py" "${models_dir}/${m}/model_inputs_false.bnet" '!{x}: 3{y}: (@{x}: ~{y} && AX {x}) && (@{y}: AX {y})'
+        timeout -v 1h python3 "${src_dir}/model_check.py" "${models_dir}/${m}/model_inputs_false.bnet" '3{x}: 3{y}: (@{x}: ~{y} && AX {x}) && (@{y}: AX {y}) && EF ({x} && !{z}: AX {z}) && EF ({y} && !{z}: AX {z}) && AX (EF ({x} && !{z}: AX {z}) ^ EF ({y} && !{z}: AX {z}))'
     else
-        timeout -v 1h python3 "${project_dir}/src/model_check.py" "${models_dir}/${m}/model.bnet" 'AF !{x}: (AX (~{x} && AF {x}))'
-        timeout -v 1h python3 "${project_dir}/src/model_check.py" "${models_dir}/${m}/model.bnet" 'AF !{x}: ((AX (~{x} && AF {x})) && (EF !{y}: EX ~AF {y}))'
-        timeout -v 1h python3 "${project_dir}/src/model_check.py" "${models_dir}/${m}/model.bnet" '!{x}: 3{y}: (@{x}: ~{y} && AX {x}) && (@{y}: AX {y})'
-        timeout -v 1h python3 "${project_dir}/src/model_check.py" "${models_dir}/${m}/model.bnet" '3{x}: 3{y}: (@{x}: ~{y} && AX {x}) && (@{y}: AX {y}) && EF ({x} && !{z}: AX {z}) && EF ({y} && !{z}: AX {z}) && AX (EF ({x} && !{z}: AX {z}) ^ EF ({y} && !{z}: AX {z}))'
+        timeout -v 1h python3 "${src_dir}/model_check.py" "${models_dir}/${m}/model.bnet" 'AF !{x}: (AX (~{x} && AF {x}))'
+        timeout -v 1h python3 "${src_dir}/model_check.py" "${models_dir}/${m}/model.bnet" 'AF !{x}: ((AX (~{x} && AF {x})) && (EF !{y}: EX ~AF {y}))'
+        timeout -v 1h python3 "${src_dir}/model_check.py" "${models_dir}/${m}/model.bnet" '!{x}: 3{y}: (@{x}: ~{y} && AX {x}) && (@{y}: AX {y})'
+        timeout -v 1h python3 "${src_dir}/model_check.py" "${models_dir}/${m}/model.bnet" '3{x}: 3{y}: (@{x}: ~{y} && AX {x}) && (@{y}: AX {y}) && EF ({x} && !{z}: AX {z}) && EF ({y} && !{z}: AX {z}) && AX (EF ({x} && !{z}: AX {z}) ^ EF ({y} && !{z}: AX {z}))'
     fi
 done
